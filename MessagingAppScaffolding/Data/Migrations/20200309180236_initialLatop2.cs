3baef1b362193fd4cb74b4e970f50ead5e20e58c
bryantb2 <bryantb2@my.lanecc.edu>
2020-03-09 19:05:59 -0700

    working on manage messages page

---
+++ MessagingAppScaffolding/Data/Migrations/20200309180236_initialLatop2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MessagingApp.Data.Migrations
+{
+    public partial class initialLatop2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
